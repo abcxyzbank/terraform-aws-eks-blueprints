9ee754f49f2010d28f6d5900920a7ff05a90d922
DELETE_ME <92442070+abcxyzbank@users.noreply.github.com>
2024-10-07 09:00:11 +0000

    Refactor backend configuration in versions.tf and update e2e-parallel-destroy.yml

--- patterns/wireguard-with-cilium/main.tf
+++ patterns/wireguard-with-cilium/main.tf
@@ -1,24 +1,4 @@
-terraform {
-  required_version = ">= 1.3"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.34"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.9"
-    }
-  }
-
-  # ##  Used for end-to-end testing on project; update to suit your needs
-  # backend "s3" {
-  #   bucket = "terraform-ssp-github-actions-state-1"
-  #   region = "us-east-1"
-  #   key    = "e2e/wireguard-with-cilium/terraform.tfstate"
-  # }
-}
 
 provider "aws" {
   region = local.region
