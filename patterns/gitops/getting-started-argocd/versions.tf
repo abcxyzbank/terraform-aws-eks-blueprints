6c99cb680a4e6f726d3606d850236f8d8524d514
DELETE_ME <92442070+abcxyzbank@users.noreply.github.com>
2024-10-07 08:04:48 +0000

    Refactor backend configuration in versions.tf

--- patterns/gitops/getting-started-argocd/versions.tf
+++ patterns/gitops/getting-started-argocd/versions.tf
@@ -18,8 +18,8 @@ terraform {
 
   # ##  Used for end-to-end testing on project; update to suit your needs
   # backend "s3" {
-  #   bucket = "terraform-ssp-github-actions-state"
-  #   region = "us-west-2"
+  #   bucket = "terraform-ssp-github-actions-state-1"
+  #   region = "us-east-1"
   #   key    = "e2e/getting-started-argocd/terraform.tfstate"
   # }
 }
